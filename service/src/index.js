24d33214c2b755ebae2f31265a9438552968d4b2
Artem <xhc044x@gmail.com>
2018-03-21 00:12:44 +0200

    Change structure of server index file

--- service/src/index.js
+++ service/src/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const database = require('./database');
 const config = require('./../config');
+const routes = require('./routes');
+/**
+ * Middlewares
+ */
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const logger = require('morgan');
 
 const {
     api: {
@@ -13,14 +21,6 @@ const {
 const app = express();
 database();
 
-/**
- * Middlewares
- */
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const helmet = require('helmet');
-const logger = require('morgan');
-
 if (process.env.ENV === 'development') {
     app.use(logger('dev'))
 }
@@ -28,11 +28,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
-
-/**
- * Routes
- */
-const routes = require('./routes');
 
 app.use('/api', routes);
 
