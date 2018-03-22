6d7d2493442bedbb6b2c9f7f020258728bb2d750
Artem <xhc044x@gmail.com>
2018-03-23 01:51:29 +0200

    move sms services to services folder

--- service/src/services/smsService.js
+++ service/src/services/smsService.js
@@ -10,13 +10,10 @@ const sendSMS = (receiverNumber, message) => {
     return new Promise((resolve, reject) => {
         nexmo.message.sendSms(config.nexmo.from, receiverNumber, message, (error, response) => {
             if(error) {
-                console.log(error);
                 reject(error);
             } else if(response.messages[0].status != '0') {
-                console.log('Nexmo returned back a non-zero status');
                 reject('Nexmo returned back a non-zero status');
             } else {
-                console.log(response);
                 resolve(response);
             }
         });
