b6a9061073a64c4da2bf410f7781d32abbcfa8a4
Artem <xhc044x@gmail.com>
2018-04-04 23:16:44 +0300

    delete prev app folder

--- app/src/store/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from 'redux';
-import volunteer from './volunteer';
-
-export default combineReducers({
-    volunteer
-});
--- app/src/store/reducers/volunteer/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import user from './user';
-
-const volunteer = combineReducers({
-  user
-});
-
-export default volunteer;
