a4c8d6264344991e49317125f3e96800944d10c8
Artem <xhc044x@gmail.com>
2018-04-04 21:21:52 +0300

    add pet status creation controller

---
+++ service/src/controllers/admin-petStatuses.js
@@ -0,0 +1,28 @@
+const httpStatus = require('http-status');
+const APIError = require('../helpers/APIError');
+const PetStatus = require('../models/petStatus');
+
+/**
+ * Controller for pet status creation.
+ * 
+ * @param {*} req  in request body name is required;
+ * @param {*} res  sends request status, created type and message
+ * @param {*} next function moves to next middleware
+ */
+const create = async (req, res, next) => {
+    try {
+        const { name } = req.body;
+        const type = await new PetStatus({ name }).saveAsync();
+        const returnObj = {
+            success: true,
+            message: 'Pet status was succesfully created.',
+            type
+        };
+        res.send(returnObj);
+    } catch (error) {
+        const err = new APIError(`Error during creating new user: ${error}`, httpStatus.INTERNAL_SERVER_ERROR);
+        next(err);
+    }
+};
+
+module.exports = { create };
