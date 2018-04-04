8ee41b98f572c456114c4638e43e0a84d1c5edc5
Artem <xhc044x@gmail.com>
2018-04-04 20:20:49 +0300

    add petType model

---
+++ service/src/models/petType.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+/**
+ * PetTypeSchema
+ */
+const PetTypeSchema = new Schema({
+    name: { type: String, required: true },
+    created: { type: Date, default: Date.now },
+    updated: { type: Date, default: null },
+});
+
+/**
+ * PetTypeSchema hook
+ * 
+ * Each time verification updated, it sets new updated value
+ */
+PetTypeSchema.pre('save', function (next) {
+    if (!this.isNew) {
+        this.updated = new Date(); 
+    }
+    next();
+});
+
+/**
+ * PetTypeModel
+ */
+const PetTypeModel = mongoose.model('PetType', PetTypeSchema);
+
+module.exports = PetTypeModel;
